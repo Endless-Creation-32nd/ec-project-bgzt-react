2a6737539841b171a5e6c26bff0c377d9d9236fc
오승태 <yeoularu@Edge-of-the-Midnight.local>
2022-07-28 16:43:33 +0900

    infinity carousel 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -23,31 +23,66 @@ function Main() {
 }
 
 function Banner() {
-  const slides = ["bg-cyan-200", "bg-yellow-200", "bg-rose-400"];
+  const data = ["bg-cyan-200", "bg-yellow-200", "bg-rose-400"];
+  const slides = [data[data.length - 1], ...data, data[0]];
+  const transitionTime = 300;
 
   const [currentIndex, setCurrentIndex] = useState(1);
+  const [transition, setTransition] = useState(`duration-${transitionTime}`);
+
+  function replaceSlide(index) {
+    setTimeout(() => {
+      setTransition("");
+      setCurrentIndex(index);
+    }, transitionTime);
+  }
+  function handleSwipe(direction) {
+    let index = currentIndex + direction;
+    setCurrentIndex(index);
+    if (index < 1) {
+      index += data.length;
+      replaceSlide(index);
+    } else if (index >= data.length + 1) {
+      index -= data.length;
+      replaceSlide(index);
+    }
+    setTransition(`duration-${transitionTime}`);
+  }
 
   return (
     <div className="relative w-[1024px] overflow-hidden">
       <button
         className="absolute top-[45%] left-[calc(50%-515px)] z-[1] text-4xl text-neutral-300"
-        onClick={() => {}}
+        onClick={async () => {
+          handleSwipe(-1);
+        }}
       >
         〈
       </button>
-      <button className="absolute top-[45%] right-[calc(50%-512px)] z-[1] text-4xl text-neutral-300">
+      <button
+        className="absolute top-[45%] right-[calc(50%-512px)] z-[1] text-4xl text-neutral-300"
+        onClick={() => handleSwipe(1)}
+      >
         〉
       </button>
 
-      <div className={`flex`}>
-        {slides.map((color, index) => (
-          <div
-            key={index}
-            className={`h-[300px] w-[1024px] flex-none ${slides[index]}`}
-          >
-            {index + 1}
-          </div>
-        ))}
+      <div>
+        <div
+          className={`flex ${transition}`}
+          style={{
+            transform: `translateX(${-100 * currentIndex}%)`,
+          }}
+        >
+          {slides.map((color, index) => (
+            <div
+              key={index}
+              className={`h-[300px] w-[1024px] flex-none ${slides[index]}`}
+            >
+              {index}
+              {currentIndex}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
