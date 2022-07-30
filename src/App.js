84b602de402740c8b52d8cd621322c2e8c1ccd80
yeoularu <yeoularu@gmail.com>
2022-07-31 00:18:21 +0900

    react-router init, NotFound page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./pages/Main";
 import Topbar from "./components/Topbar";
 import Footer from "./components/Footer";
+import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <div className="min-w-[1236px]">
-        <Topbar />
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="min-w-[1236px]">
+          <Topbar />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route path="/products/:productId" element={<Product />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,3 +1,16 @@
-function Product() {
-  return <div></div>;
+import { useParams } from "react-router-dom";
+
+function Product(props) {
+  const { productId } = useParams();
+  return (
+    <section className="flex justify-center">
+      <div className="w-[1024px]">
+        <div>
+          <div className="flex px-[30px]">{productId}</div>
+        </div>
+      </div>
+    </section>
+  );
 }
+
+export default Product;
